8d75d241d947eadfaab98585e7a9e06b445d8f9b
Elnara Malikzade <12728@holbertonstudents.com>
2026-07-01 12:41:32 +0400

    Add task3

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,19 +1,19 @@
-import User from '../models/User.js';
+import User from '../models/User';
 
 class UsersController {
-    static async postNew(req, res) {
-        const { email, password } = req.body;
-        if (!email) {
-            res.status(400).json({ error: 'Missing email'});
-        } else if (!password) {
-            res.status(400).json({ error: 'Missing password'});
-        } else if (await User.findByEmail(email)) {
-            res.status(400).json({ error: 'Already exist'});
-        } else {
-            const newUser = await User.create(email, password);
-            res.status(201).json({ id: newUser.id, email: newUser.email});
-            newUser.save();
-        }
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+      if (!email) {
+        res.status(400).json({ error: 'Missing email' });
+      } else if (!password) {
+        res.status(400).json({ error: 'Missing password' });
+      } else if (await User.findByEmail(email)) {
+        res.status(400).json({ error: 'Already exist' });
+      } else {
+        const newUser = await User.create(email, password);
+        res.status(201).json({ id: newUser.id, email: newUser.email });
+        newUser.save();
     }
+  }
 }
 export default UsersController;
