afc01f6c314727ec68e9a8e4d3bf1a9d6cf685f8
Elnara Malikzade <12728@holbertonstudents.com>
2026-07-01 14:14:00 +0400

    Add task4

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,8 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 import sha1 from 'sha1';
-import redisClient from '../utils/redis.mjs';
-import dbClient from '../utils/db.mjs';
-
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
 class AuthController {
   static async getConnect(req, res) {
@@ -39,17 +38,24 @@ class AuthController {
 
     return res.status(200).json({ token });
   }
-  static async getDisconnect(req, res){
+
+  static async getDisconnect(req, res) {
     const userToken = req.header('X-Token');
+
     if (!userToken) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
+
     const userId = await redisClient.get(`auth_${userToken}`);
+
     if (!userId) {
       return res.status(401).json({ error: 'Unauthorized' });
-    }   
+    }
+
     await redisClient.del(`auth_${userToken}`);
+
     return res.status(204).end();
+  }
 }
-}
+
 export default AuthController;
